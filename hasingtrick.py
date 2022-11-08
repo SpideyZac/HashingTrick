0230701df2627a41ad86cf12a1ddf48d37bc7a2f
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2022-11-08 14:17:10 -0800

    Update hasingtrick.py

--- hasingtrick.py
+++ hasingtrick.py
@@ -6,7 +6,6 @@ def set_hash_seed(seed: str) -> None:
 def hash_inputs(inputs: list[float], num_features: int) -> list[float]:
   # https://en.wikipedia.org/wiki/Feature_hashing#:~:text=In%20machine%20learning%2C%20feature%20hashing%2C%20also%20known%20as,features%20into%20indices%20in%20a%20vector%20or%20matrix.
   x = [0 for _ in range(num_features)]
-  x = tuple(x)
   
   for i, item in enumerate(inputs):
     h = hash((i, item))
